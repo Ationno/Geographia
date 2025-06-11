bec6ee58c02f0d80bf55534b335a9cc5747c582d
Antonio Felix Glorioso Ceretti <antonioglorioso1@hotmail.com>
2025-06-11 15:44:17 -0300

    Agregado Backend y Frontend
    Se utilizara Angular para el Frontend y Node.js/express para Backend

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+// backend/index.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor Node.js funcionando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
